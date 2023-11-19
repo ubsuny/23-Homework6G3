cc7257498f8d6c74c518ec100a486e07b88db8c6
Reshna <reshnash@buffalo.edu>
2023-11-18 20:46:13 -0500

    Define root secant function.

--- calculus.py
+++ calculus.py
@@ -116,4 +116,33 @@ def root_simple(f, x, dx, accuracy=1.0e-6, max_steps=1000, root_debug=False):
             iterations.append([x,fx])
     return x,np.array(iterations)
 
+def root_secant(f, x0, x1, accuracy=1.0e-6, max_steps=20, root_debug=False):
+    """Return root of f(x) given guesses x0 and x1 with specified accuracy.
+    Uses secant root-finding algorithm.
+    """
+    iterations = []
+    f0 = f(x0)
+    dx = x1 - x0
+    step = 0
+    if root_debug:
+        iterations.append([x0, f0])
+    if f0 == 0:
+        return x0
+    while abs(dx) > abs(accuracy):
+        f1 = f(x1)
+        if f1 == 0:
+            return x1
+        if f1 == f0:
+            raise Exception("Secant horizontal f(x0) = f(x1) algorithm fails")
+        dx *= -f1 / (f1 - f0)
+        x0 = x1
+        f0 = f1
+        x1 += dx
+        step += 1
+        if step > max_steps:
+            root_max_steps("root_secant", max_steps)
+        if root_debug:
+            iterations.append([x1, f1])
+    return x1, np.array(iterations)
+
 
