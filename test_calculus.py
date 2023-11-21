4dfdb23def8fd7f4d87e9019781bca408d19c090
Reshna <reshnash@buffalo.edu>
2023-11-21 01:31:17 -0500

    Add libraries and import functions.

--- test_calculus.py
+++ test_calculus.py
@@ -4,3 +4,11 @@ test_calculus.py
 This module provides unit tests for the calculus module.
 
 """
+import unittest
+import math
+import numpy as np
+from calculus import (
+    simpson, trapezoid, adaptive_trapezoid, 
+    root_simple, root_bisection, root_secant, 
+    root_tangent, root_print_header, root_print_step, root_max_steps
+)
